8daf6a64f519ad261c374b10d5a9843ff6a0b345
laroccol <lucas.larocco@gmail.com>
2024-04-15 19:11:34 -0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { H1 } from "@/components/typography/H1";
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-4">
+      <H1>Lucas' TODO</H1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
