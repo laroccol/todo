766629ab345e71ced841e94324035b0563b1e8a4
laroccol <lucas.larocco@gmail.com>
2024-04-15 19:23:49 -0500

    add turbo, fix &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-4">
-      <H1>Lucas' TODO</H1>
+      <H1>Lucas&apos; TODO</H1>
       <Button>Click me</Button>
     </main>
   );
